7b2d0646376be06d51ac1fa144961fe4e5f36df5
Colin Gillespie <csgillespie@gmail.com>
2015-08-19 10:35:23 +0100

    Various typos fixed. Added section on Rcpp sugar.

--- notes/src/mean_c.cpp
+++ notes/src/mean_c.cpp
@@ -12,3 +12,19 @@ double mean_c(NumericVector x){
   }
   return mean;
 }
+
+// [[Rcpp::export]]
+NumericVector res_c(NumericVector x, NumericVector y){
+  int i;
+  int n = x.size();
+  NumericVector residuals(n);
+  for(i=0; i<n; i++){
+    residuals[i] = pow(x[i] - y[i], 2);
+  }
+  return residuals;
+}
+
+// [[Rcpp::export]]
+NumericVector res_sugar(NumericVector x, NumericVector y){
+  return pow(x-y, 2);
+}

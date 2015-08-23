cc1106dedef5bedd316575608c25c6781133ede8
Robinlovelace <rob00x@gmail.com>
2015-08-23 23:57:50 +0100

    Updates from RL

--- functions/rmd_bind.R
+++ functions/rmd_bind.R
@@ -1,7 +1,6 @@
 # Book building function
 rmd_bind <- function(dir = ".",
-  book_header = readLines(textConnection("---\ntitle: 'Title'\n---")),
-  chap_ord = NULL){
+  book_header = readLines(textConnection("---\ntitle: 'Title'\n---"))){
   old <- setwd(dir)
   on.exit(setwd(old))
   if(length(grep("book.Rmd$", list.files())) > 0){
@@ -9,7 +8,9 @@ rmd_bind <- function(dir = ".",
   }
   cfiles <- list.files(pattern = "*.Rmd")
   cfiles <- cfiles[-grep("book", cfiles)]
-  if(!is.null(chap_ord)) cfiles <- cfiles[chap_ord] # chapter order
+  if(exists("chap_ord")){
+    cfiles <- cfiles[chap_ord] # chapter order
+  }
   write(book_header, file = "book.Rmd")
   ttext <- NULL
   for(i in 1:length(cfiles)){

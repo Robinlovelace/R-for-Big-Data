fb9df16a8ae2d81a2b1bc50808928079388715f7
Colin Gillespie <csgillespie@gmail.com>
2015-08-25 15:42:30 +0100

    Moving header to separate file

--- build.R
+++ build.R
@@ -1,23 +1,9 @@
+source("functions/rmd_bind.R")
 # Aim: build the tutorial from its constituent parts
 (cfiles <- list.files(pattern = "*.Rmd$"))
-book_header <- readLines(textConnection(
-'---
-title: "R for Big Data"
-author: "Colin Gillespie and Robin Lovelace"
-date: "February 8, 2015"
-classoption: oneside
-output:
-  rticles::tufte_ebook:
-    toc: true
-    latex_engine: pdflatex
-    toc_depth: 3
-    number_sections: true
-    keep_tex: true
----'
-))
 
 
-source("functions/rmd_bind.R")
-rmd_bind(book_header = book_header)
+
+rmd_bind(book_header = "00-header.Rmd")
 
 rmarkdown::render("book.Rmd")
--- functions/rmd_bind.R
+++ functions/rmd_bind.R
@@ -1,6 +1,7 @@
 # Book building function
 rmd_bind <- function(dir = ".",
-  book_header = readLines(textConnection("---\ntitle: 'Title'\n---"))){
+                     book_header = readLines(textConnection("---\ntitle: 'Title'\n---")),
+                     render=TRUE){
   old <- setwd(dir)
   on.exit(setwd(old))
   if(length(grep("book.Rmd$", list.files())) > 0){
@@ -8,10 +9,11 @@ rmd_bind <- function(dir = ".",
   }
   cfiles <- list.files(pattern = "*.Rmd")
   cfiles <- cfiles[-grep("book", cfiles)]
+  cfiles <- cfiles[-grep("00-header.Rmd", cfiles)]
   if(exists("chap_ord")){
     cfiles <- cfiles[chap_ord] # chapter order
   }
-  write(book_header, file = "book.Rmd")
+  write(readLines(book_header), file = "book.Rmd")
   ttext <- NULL
   for(i in 1:length(cfiles)){
     text <- readLines(cfiles[i])
@@ -19,5 +21,7 @@ rmd_bind <- function(dir = ".",
     if(length(hspan > 1)) text <- text[-c(hspan[1]:hspan[2])]
     write(text, sep = "\n", file = "book.Rmd", append = TRUE)
   }
+  if(render)
+    rmarkdown::render("book.Rmd")
   #     render("book.Rmd", output_format = "pdf_document")
 }

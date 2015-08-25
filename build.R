2b70f71e0e79b383dbac2ea98d90b393eab2f088
Colin Gillespie <csgillespie@gmail.com>
2015-08-25 14:40:03 +0100

    Move to ebook

--- build.R
+++ build.R
@@ -1,11 +1,18 @@
 # Aim: build the tutorial from its constituent parts
 (cfiles <- list.files(pattern = "*.Rmd$"))
-
 book_header <- readLines(textConnection(
 '---
 title: "R for Big Data"
 author: "Colin Gillespie and Robin Lovelace"
-output: rmarkdown::tufte_handout
+date: "February 8, 2015"
+classoption: oneside
+output:
+  rticles::tufte_ebook:
+    toc: true
+    latex_engine: pdflatex
+    toc_depth: 3
+    number_sections: true
+    keep_tex: true
 ---'
 ))
 

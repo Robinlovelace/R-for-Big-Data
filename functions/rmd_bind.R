768e595e4353a222b5962359e5f9e5b6b2542780
Colin Gillespie <csgillespie@gmail.com>
2015-08-27 17:23:19 +0100

    Removed warning, since we can't do anything about it. Moved the `write` to the top. That way we avoid the grep bug when book.Rmd doesn't exist.

--- functions/rmd_bind.R
+++ functions/rmd_bind.R
@@ -1,16 +1,17 @@
 # Book building function
 rmd_bind <- function(book_header, dir = ".", render=TRUE){
   old <- setwd(dir);  on.exit(setwd(old))
-  if(length(grep("book.Rmd$", list.files())) > 0){
-    warning("book.Rmd already exists")
-  }
+  write(readLines(book_header), file = "book.Rmd")
+
   cfiles <- list.files(pattern = "*.Rmd")
-  cfiles <- cfiles[-grep("book", cfiles)]
+  cfiles <- cfiles[-grep("book.Rmd", cfiles)]
   cfiles <- cfiles[-grep("00-header.Rmd", cfiles)]
+
+
   if(exists("chap_ord")){
     cfiles <- cfiles[chap_ord] # chapter order
   }
-  write(readLines(book_header), file = "book.Rmd")
+
   ttext <- NULL
   for(i in 1:length(cfiles)){
     text <- readLines(cfiles[i])

736e0054ffc7f0a4278b78066b8d6a32d5e0cacf
Colin Gillespie <csgillespie@gmail.com>
2015-09-06 11:44:01 +0100

    List of R packages used in book

---
+++ packages.R
@@ -0,0 +1,37 @@
+## CRAN packages needed
+pkgs = c(
+  "drat", "dplyr", "readxl",
+  "readr", "gdata", "openxlsx",
+  "downloader", "tidyr", "Rcpp",
+  "pryr", "grid", "png",
+  "ff", "ffbase", "biglm", "xtable"
+)
+## Github packages
+github_pkgs = c("r4bd")
+
+
+## create the data frames
+pkgs = data.frame(pkg = pkgs,
+                  repo = "http://cran.rstudio.com/",
+                  installed = pkgs %in% installed.packages(),
+                  stringsAsFactors = FALSE)
+repo = drat::addRepo("rcourses")["rcourses"]
+
+github_pkgs = data.frame(pkg = github_pkgs,
+                         repo = repo,
+                         installed = github_pkgs %in% installed.packages(),
+                         stringsAsFactors = FALSE, row.names=NULL)
+
+## Combine all data frames of package info
+pkgs = rbind(pkgs, github_pkgs)
+
+## Update packages
+update.packages(checkBuilt = TRUE, ask = FALSE,
+                repos = unique(pkgs$repo),
+                oldPkgs = pkgs$pkg)
+
+## Install missing packages
+to_install = pkgs[!pkgs$installed,]
+if(nrow(to_install))
+  install.packages(to_install$pkg, repos = to_install$repo)
+~

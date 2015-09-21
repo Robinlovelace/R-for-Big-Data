feaaf196c81de334c7a6a00152eb5345f7abed5e
Robinlovelace <rob00x@gmail.com>
2015-09-21 23:33:17 +0100

    Add files to build with bookdown

--- build.R
+++ build.R
@@ -1,26 +1,73 @@
 # Which chapters do you want to build?
-chap_ord = 1:11
+chap_ord = 1
 
-# Book building function
-rmd_bind <- function(book_header, dir = ".", render=TRUE, chap_ord = NULL){
-  old <- setwd(dir);  on.exit(setwd(old))
-  write(readLines(book_header), file = "book.Rmd")
+library(bookdown)
+library(rmarkdown)
 
-  cfiles <- list.files(pattern = "*.Rmd")
-  cfiles <- cfiles[-grep("book.Rmd", cfiles)]
-  cfiles <- cfiles[-grep("00-header.Rmd", cfiles)]
 
-  if(!is.null(chap_ord)) cfiles = cfiles[chap_ord]
-
-  for(i in 1:length(cfiles)){
-    ttext <- readLines(cfiles[i])
-    hspan <- grep("---", ttext)
-    if(length(hspan > 1)) ttext <- ttext[-(hspan[1]:hspan[2])]
-    write(ttext, sep = "\n", file = "book.Rmd", append = TRUE)
-  }
-  if(render)
-    rmarkdown::render("book.Rmd")
+# Render chapters into tex  ----------------------------------------------------
+needs_update <- function(src, dest) {
+  if (!file.exists(dest)) return(TRUE)
+  mtime <- file.info(src, dest)$mtime
+  mtime[2] < mtime[1]
 }
 
-rmd_bind(book_header = "00-header.Rmd", chap_ord = chap_ord)
+parse_md <- function(in_path) {
+  out_path <- tempfile()
+  on.exit(unlink(out_path))
+  cmd <- paste0("pandoc -f ", markdown_style, " -t json -o ", out_path, " ", in_path)
+  system(cmd)
+
+  RJSONIO::fromJSON(out_path, simplify = FALSE)
+}
+
+extract_headers <- function(in_path) {
+  x <- parse_md(in_path)
+  body <- x[[2]]
+  ids <- vapply(headers, id, FUN.VALUE = character(1))
+  ids[ids != ""]
+}
+
+render_chapter <- function(src) {
+  dest <- file.path("book/tex/", gsub("\\.rmd", "\\.tex", src))
+  if (!needs_update(src, dest)) return()
+
+  message("Rendering ", src)
+  command <- bquote(rmarkdown::render(.(src), bookdown::tex_chapter(),
+    output_dir = "book/tex", quiet = TRUE, env = globalenv()))
+  writeLines(deparse(command), "run.r")
+  on.exit(unlink("run.r"))
+  source_clean("run.r")
+}
+
+source_clean <- function(path) {
+  r_path <- file.path(R.home("bin"), "R")
+  cmd <- paste0(shQuote(r_path), " --quiet --file=", shQuote(path))
+
+  out <- system(cmd, intern = TRUE)
+  status <- attr(out, "status")
+  if (is.null(status)) status <- 0
+  if (!identical(as.character(status), "0")) {
+    stop("Command failed (", status, ")", call. = FALSE)
+  }
+}
+
+chapters <- list.files(pattern = "*.Rmd")[chap_ord]
+lapply(chapters, render_chapter)
+
+# Copy across additional files -------------------------------------------------
+file.copy("book/r-for-big-data.tex", "book/tex/", recursive = TRUE)
+file.copy("diagrams/", "book/tex/", recursive = TRUE)
+file.copy("screenshots/", "book/tex/", recursive = TRUE)
+
+# Build tex file ---------------------------------------------------------------
+# (build with Rstudio to find/diagnose errors)
+old <- setwd("book/tex")
+system("xelatex -interaction=batchmode r-for-big-data ")
+system("xelatex -interaction=batchmode r-for-big-data ")
+setwd(old)
+
+file.copy("book/tex/r-for-big-data.pdf", "book/r-for-big-data.pdf", overwrite = TRUE)
+
+# Build website
 
